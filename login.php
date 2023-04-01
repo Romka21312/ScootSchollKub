087f9f0485ff87cf7a38b9c37467073cfa76235c
Romka21312 <106865957+Romka21312@users.noreply.github.com>
2023-04-01 22:43:12 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UFT-8">
+    <tittle>Админка</tittle>
+</head>
+<body>
+    <h2 style="text-aligin:center;padding-top:100px,padding-left:25%">Вход в панель администратора</h2>
+</body>
+</html>
